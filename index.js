37b71e7592038ffe826332783cea3b88c4e4c499
candy <130109889+ro501@users.noreply.github.com>
2023-07-18 18:36:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ flag = false;
 function usera(){
 if (user == username.value && passwd == password.value){
     message.innerHTML = `<h1>Login successful</h1> <h1> congratulations </h1>
-    <h2 align="left" style="padding: 30px; margin:20px">Congratulations on logging into our web page! We are so glad to have you as a member of our community. We hope you enjoy all that our site has to offer.<br>
+    <h2 align="center" style="padding: 30px; margin:20px">Congratulations on logging into our web page! We are so glad to have you as a member of our community. We hope you enjoy all that our site has to offer.<br>
      We know that you have worked hard to get to this point, and we are proud of you for your dedication. We are excited to see what you will accomplish in the future.<br>
      Thank you for choosing our site. We look forward to working with you!<br>
      Sincerely,<br>
